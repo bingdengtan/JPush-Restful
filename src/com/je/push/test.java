e75f191c7c22b39cc02348d1c86d43df45825bdd
bingdengtan <bingdeng.tan@163.com>
2017-11-06 00:30:02 +0800

    init my project

---
+++ src/com/je/push/test.java
@@ -0,0 +1,12 @@
+package com.je.push;
+import com.je.push.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JiguangPush jp = new JiguangPush();
+		//jp.push("", "hello");
+	}
+
+}
